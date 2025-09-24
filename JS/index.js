c62c79fbc175c2228325c4c416aff39c448a017b
garvsinghal947@gmail.com <garvhome@Garvs-MacBook-Air.local>
2025-09-24 10:27:48 +0530

    JSfunction

---
+++ JS/index.js
@@ -0,0 +1,89 @@
+// console.log("hello world");
+//  console.log("a="+a);
+ 
+//  var a =40;
+
+//  let b = '20';
+//  if(a==20){
+//     console.log("equal data");
+//  }
+//  else{
+//     console.log("unequal data");
+    
+//  }
+
+// const name = "rahul";
+// const data = `hiiiiiii........${name}`;
+// console.log(data);
+
+
+// function msg(name){
+//     return name+"welcome to js";
+// }
+
+// const data = msg("Ram ");
+// console.log(data);
+
+
+// const data = function msg(name){
+//     return name+"welcome to js";
+// }
+
+// const msg = data("Rahul ")
+// console.log(msg);
+
+// const data = (name) => {
+//     return name+ "welcome to js";
+// }
+
+// const msg = data("Rahul ");
+// console.log(msg);
+
+// const data = msg => "Hiiii......"+msg;
+// console.log(data("Greetings....."));
+
+// (function(){
+//     console.log("Welcome to JS");
+// })();
+
+// (()=>{
+//     console.log("Ayussssssssss");
+// })();
+
+// setTimeout(()=>{
+//     console.log("Hii.....I am using Timeout ");
+    
+// },1000);
+
+// setInterval(()=>{
+//     console.log("hiiii........how r u?");
+    
+// },1000);
+// function greeting(msg = "Welcome"){
+//     console.log("Hiii"+msg);
+    
+// }
+// greeting("Rahul");
+
+// function selectlanguage(lang){
+//     let data;
+//     if(lang=='java'){
+//         function javaCompiler(){
+//             return "java compiler selected";
+//         }
+//         data = javaCompiler();
+//     }else if(lang=='c'){
+//         function cCompiler(){
+//             return 'C Compiler selected'
+//         }
+//         data = cCompiler();
+//     }else{
+//         data = "no such compiler available"
+//     }
+
+//     return data;
+// }
+
+// const msg = selectlanguage('anaconda');
+// console.log(msg);
+
