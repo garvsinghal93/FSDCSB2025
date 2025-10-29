0573b3e793ae3a9992dd294e088f1d0dd4092578
garvsinghal947@gmail.com <garvhome@Garvs-MacBook-Air.local>
2025-10-29 10:23:00 +0530

    React + vite Component

---
+++ REACTWORK/reactviteapp/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Profile from './component/Profile'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <h2>Welcome to React Vite</h2>
+      <h3>
+        <Profile/>
+      </h3>
+    </div>
+  )
+}
+
+export default App
---
+++ REACTWORK/reactviteapp/src/component/Profile.jsx
@@ -0,0 +1,14 @@
+import './profile.css';
+
+function Profile() {
+  return (
+    <div className='parent'>
+        <h2>Profile Page</h2>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPMzFiLWMlh6RV2yQ10INO0BWQtmwfr0i0Mg&s"></img>
+        <h2>Lakshay</h2>
+        <h2>CS B</h2>
+        <h2>ABES Engineering College</h2>
+    </div>
+  )
+}
+export default Profile
